224b958ba24b62dc0cffd245c48a492f624f7e33
Thiago Silva Dos Santos <79120762+Thico10@users.noreply.github.com>
2021-07-03 22:51:18 -0300

    atualização 03/07/2021

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -14,3 +14,46 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+   /* function informacao(){ 
+        window.alert (innerHtml='Lançado em 2008 pela Waner Bros Pictures e DC Comics : ' + 
+        'Christopher Nolan - Director, Screenplay, Story, ' + 
+        'Bill Finger, Jerry Robinson, Bob Kane - Characters, '+
+    'Jonathan Nolan - Screenplay, ' +
+    'David S. Goyer - Story, ' + 'Elenco: Christian Bale - Bruce Wayne / Batman, Heath Ledger - Joker, Michael Caine - Alfred Pennyworth, Gary Oldman - Commissioner James Gordon, Aaron Eckhart - Harvey Dent / Two-Face, Maggie Gyllenhaal - Rachel Dawes, Morgan Freeman - Lucius Fox, Nestor Carbonell - Mayor Anthony Garcia, Monique Gabriela Curnen - Anna Ramirez. ' +' Gênero: Ação/Fantasia/Herois')
+    
+ } */
+ var area = window.document.getElementById('info')
+ area.addEventListener('click',clicar)
+ area.addEventListener('mouseout',sair)
+ 
+ function clicar() {
+     area.innerText = 'Lançado em 2008 pela Waner Bros Pictures e DC Comics : ' + 
+     'Christopher Nolan - Director, Screenplay, Story, ' + 
+     'Bill Finger, Jerry Robinson, Bob Kane - Characters, '+
+ 'Jonathan Nolan - Screenplay, ' +
+ 'David S. Goyer - Story, ' + 'Elenco: Christian Bale - Bruce Wayne / Batman, Heath Ledger - Joker, Michael Caine - Alfred Pennyworth, Gary Oldman - Commissioner James Gordon, Aaron Eckhart - Harvey Dent / Two-Face, Maggie Gyllenhaal - Rachel Dawes, Morgan Freeman - Lucius Fox, Nestor Carbonell - Mayor Anthony Garcia, Monique Gabriela Curnen - Anna Ramirez. ' +' Gênero: Ação/Fantasia/Herois'
+ 
+ }
+ function sair() {
+    area.innerHTML = document.getElementById('info')
+    area.innerText = 'Mais Informações'
+   
+ }
+ var bat = window.document.getElementById('bat')
+ bat.addEventListener('mouseenter',entrar)
+ bat.addEventListener('mouseout',sair)
+
+ 
+ 
+
+ function entrar() {
+     bat.innerText ='Em Gotham City o milionário Bruce Wayne, que quando jovem teve os pais assassinados por bandidos, resolve combater o crime como Batman, o Homem-Morcego. Mas chega o dia em que o vilão Coringa decide dominar a cidade e se torna um grande desafio para o super-herói.'
+     
+    }
+
+
+
+
+ 
