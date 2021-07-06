3e33479ac0c2714d14f99dfd3d9ae777c4be5f42
Thiago Silva Dos Santos <79120762+Thico10@users.noreply.github.com>
2021-07-05 23:26:01 -0300

    atualização 05/07/2021

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -29,15 +29,15 @@ $('.owl-carousel').owlCarousel({
  area.addEventListener('mouseout',sair)
  
  function clicar() {
-     area.innerText = 'Lançado em 2008 pela Waner Bros Pictures e DC Comics : ' + 
+    alert(  'Lançado em 2008 pela Waner Bros Pictures e DC Comics : ' + 
      'Christopher Nolan - Director, Screenplay, Story, ' + 
      'Bill Finger, Jerry Robinson, Bob Kane - Characters, '+
  'Jonathan Nolan - Screenplay, ' +
  'David S. Goyer - Story, ' + 'Elenco: Christian Bale - Bruce Wayne / Batman, Heath Ledger - Joker, Michael Caine - Alfred Pennyworth, Gary Oldman - Commissioner James Gordon, Aaron Eckhart - Harvey Dent / Two-Face, Maggie Gyllenhaal - Rachel Dawes, Morgan Freeman - Lucius Fox, Nestor Carbonell - Mayor Anthony Garcia, Monique Gabriela Curnen - Anna Ramirez. ' +' Gênero: Ação/Fantasia/Herois'
- 
+    )
  }
  function sair() {
-    area.innerHTML = document.getElementById('info')
+    area.document.createElement("i")
     area.innerText = 'Mais Informações'
    
  }
@@ -45,15 +45,28 @@ $('.owl-carousel').owlCarousel({
  bat.addEventListener('mouseenter',entrar)
  bat.addEventListener('mouseout',sair)
 
- 
- 
 
  function entrar() {
      bat.innerText ='Em Gotham City o milionário Bruce Wayne, que quando jovem teve os pais assassinados por bandidos, resolve combater o crime como Batman, o Homem-Morcego. Mas chega o dia em que o vilão Coringa decide dominar a cidade e se torna um grande desafio para o super-herói.'
-     
+     bat.document.createElement("div")
     }
+    function sair() {
+        bat.innerText = 'Mais Informações'
+        
+    }    
 
+    var aranha = window.document.getElementById('aranha')
+    bat.addEventListener('mouseenter',entrar)
+    bat.addEventListener('mouseout',sair)
+   
+   
+    function entrar() {
+        bat.innerText ='Peter Parker é um jovem estudioso que vive com seus tios, Ben e May, desde que seus pais faleceram. Peter tem dificuldade em se relacionar com seus colegas, por ser tímido e por eles o considerarem um nerd. Até que, em uma demonstração científica, um acidente inesperado faz com que uma aranha modificada geneticamente pique Peter. A partir de então seu corpo é quimicamente alterado pela picada da aranha.'
+      
+       }
+       function sair() {
+           bat.innerText = 'Mais Informações'
+           
+       }    
 
-
-
- 
+   
